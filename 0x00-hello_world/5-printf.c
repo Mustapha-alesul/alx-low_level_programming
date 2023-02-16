36fceb60ba71f49573ca90b468b2f05222d72eef
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-16 13:35:46 -0800

    printing with printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - pinting using printf
+ * Return: 0 Value to the main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
