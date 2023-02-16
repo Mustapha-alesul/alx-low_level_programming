1c1feeafc5ebcac12942facf43ea2b4044b303d5
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-16 13:16:09 -0800

    using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - will be used to print
+ * write a code print a string
+ * but with using puts instead of printf
+ * Return: 0 value
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
