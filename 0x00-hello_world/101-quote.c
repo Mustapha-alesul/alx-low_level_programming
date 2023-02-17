e81c560f7fad2efd71da96a4c438aefd5e80b2ef
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-16 16:51:10 -0800

    last one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - a C program that prints using putchar
+ * Return: will return 1 value
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Ko
+rpar, 2015-10-19\n",	stdout);
+	return (1);
+}
