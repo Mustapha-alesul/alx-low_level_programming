e72c90a2e35bf4831d2bafba4e40acaf97a88515
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-17 18:36:30 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - a program that prints all singl
+ * digit numbers of base 10 starting from 0
+ * Return: 0 value
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+	printf("%d", m);
+	}
+	printf("\n");
+
+	return (0);
+}
