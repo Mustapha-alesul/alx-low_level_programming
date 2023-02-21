4d58d5f84ab462a65ac9de83feb4fb8f9b411dce
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-21 13:04:11 -0800

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,20 @@
 #include "main.h"
-
 /**
- * _abs - a function that computes the absolute value of an integer
- * @m: character
- * Return: interger
+ * _abs - program to execute absolute value of a intiger
+ * @m: character to be executed
+ * Return: return any absolute value of number or m
  */
 
 int _abs(int m)
 {
-	int abs = m * -1;
+
 
 	if (m < 0)
 	{
-	_putchar(abs);
-	return (abs);
+		int absolute;
+
+		absolute = m * -1;
+		return (absolute);
 	}
-	else
-	{
-	_putchar(m);
 	return (m);
-	}
 }
