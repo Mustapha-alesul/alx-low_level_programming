d2f5d168735261e5a1261e21268df0612b0b901c
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-21 09:10:23 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,14 +10,14 @@ void print_alphabet_x10(void)
 	char alph = 'a';
 	int i = 0;
 
-	while (i <= 10)
+	while (i < 10)
 	{
 		while (alph <= 'z')
 		{
 		_putchar(alph);
 		alph++;
 		}
-	_putchar('\n');
 	i++;
+	_putchar('\n');
 	}
 }
