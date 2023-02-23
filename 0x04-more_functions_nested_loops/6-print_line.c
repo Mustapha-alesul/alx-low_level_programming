fdeba40d70c93521bc6704b29b57d3bb6b7cd7a0
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-23 11:13:03 -0800

    6-print_line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,13 @@ void print_line(int n)
 	int i;
 
 		
-		if (n >0)
+		if (n > 0)
 		{
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-	_putchar('_');
+	_putchar(95);
 	}
 	}
 		_putchar(36);
 		_putchar('\n');
 }
-
