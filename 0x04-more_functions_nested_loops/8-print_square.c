b9af67b6d3027a4961214f3a6d6be5149fc234cc
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-23 12:53:27 -0800

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - a function that prints a squarie
+ * @size: character
+ * Return: size value
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+	for (j = 1; j <= size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+}
