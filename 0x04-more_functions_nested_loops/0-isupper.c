7c71c1dec3ebf48e9054612d3ccf851ba9faf0e5
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-02-23 05:51:10 -0800

    0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character.
+ * @c: character
+ * Return: 1 value or 0
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+a readme file on 0x04. C - More functions, more nested loops
