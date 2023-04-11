fe618e921c8bdec5354913d2ccc38a4794586c1a
Mustapha-alesul <Mustapha.bdsh@gmail.com>
2023-04-11 07:03:34 -0700

    function that returns the value of a bit at a given index.

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int MASK = 1UL << index;
-
 	const unsigned long int BITS_PER_LONG = sizeof(unsigned long int) * 8;
-	if (index >= BITS_PER_LONG) {
+
+	if (index >= BITS_PER_LONG)
+	{
 		return (-1);
 	}
 	return ((n & MASK) ? 1 : 0);
